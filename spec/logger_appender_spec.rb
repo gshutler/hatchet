a33e6c3f6310223d711b63372ac1c1be81cfa1f9
Garry Shutler <garry@robustsoftware.co.uk>
2012-07-08 16:56:47 +0100

    Logger appender tests
    Added tests to ensure the LoggerAppender manipulates and delegates to the
    standard Logger it encapsulates as expected.

---
+++ spec/logger_appender_spec.rb
@@ -0,0 +1,72 @@
+# -*- encoding: utf-8 -*-
+
+require_relative 'spec_helper'
+require 'ostruct'
+
+describe LoggerAppender do
+  let(:logger)    { LoggerDouble.new }
+  let(:formatter) { SimpleFormatter.new }
+  let(:subject) do
+    LoggerAppender.new do |appender|
+      appender.logger    = logger
+      appender.formatter = formatter
+      appender.levels    = { nil => :info }
+    end
+  end
+
+  describe 'creating an appender' do
+    before { subject }
+
+    it 'removes filtering from the interal logger' do
+      assert_equal ::Logger::DEBUG, logger.level
+    end
+
+    it 'sets a formatter that returns the message with a trailing linebreak' do
+      msg = 'Plain message'
+      formatted_message = logger.formatter.call('sev', Time.now, 'prog', msg)
+      assert_equal "#{msg}\n", formatted_message
+    end
+  end
+
+  describe 'sending a message for a disabled level' do
+    it 'does not pass the message on to the logger' do
+      subject.add :debug, 'Context', 'Hello, World'
+      assert_empty logger.messages
+    end
+  end
+
+  describe 'sending a message of an enabled level' do
+    let(:message) { logger.messages.last }
+
+    it 'passes the message on to the logger' do
+      subject.add :info, 'Context', 'Hello, World'
+
+      assert :info,          message.level
+      assert 'Context',      message.context
+      assert 'Hello, World', message.message
+    end
+  end
+end
+
+class LoggerDouble
+  attr_accessor :level
+  attr_accessor :formatter
+  attr_reader :messages
+
+  def initialize
+    @messages = []
+  end
+
+  [:debug, :info, :warn, :error, :fatal].each do |level|
+    define_method level do |message|
+      messages << OpenStruct.new(level: level, message: message)
+    end
+  end
+end
+
+class SimpleFormatter
+  def format(level, context, message)
+    OpenStruct.new(level: level, context: context, message: message)
+  end
+end
+
