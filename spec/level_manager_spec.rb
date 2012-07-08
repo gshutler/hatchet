c3fec71eb9670798f64f6e3e9d628a961bd5dfae
Garry Shutler <garry@robustsoftware.co.uk>
2012-07-08 15:34:57 +0100

    Level manager override tests
    Added tests that verify that raising the threshold for a child context both
    cascades to its children and does not affect its parents.

--- spec/level_manager_spec.rb
+++ spec/level_manager_spec.rb
@@ -21,7 +21,7 @@ describe LevelManager do
       refute @manager.enabled?(:debug, 'Foo::Bar')
     end
 
-    describe 'overriding the default for a specific context' do
+    describe 'lowering the threshold for a specific context' do
       before do
         @manager.level :debug, 'Foo::Bar'
       end
@@ -36,6 +36,28 @@ describe LevelManager do
 
       it 'has debug message disabled for the parent context' do
         refute @manager.enabled?(:debug, 'Foo')
+      end
+    end
+
+    describe 'raising the threshold for a specific context' do
+      before do
+        @manager.level :fatal, 'Foo::Bar'
+      end
+
+      it 'has info messages disabled for that context' do
+        refute @manager.enabled?(:info, 'Foo::Bar')
+      end
+
+      it 'has fatal message enabled for that context' do
+        assert @manager.enabled?(:fatal, 'Foo')
+      end
+
+      it 'has info messages disabled for a child context' do
+        refute @manager.enabled?(:info, 'Foo::Bar::Baz')
+      end
+
+      it 'has info message enabled for the parent context' do
+        assert @manager.enabled?(:info, 'Foo')
       end
     end
   end
