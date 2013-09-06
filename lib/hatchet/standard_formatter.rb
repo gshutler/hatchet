b6cde7a2c98ae3730937f65a8f4fcee08405cf3c
Gavin Stark <gavin@gstark.com>
2013-09-06 16:14:55 -0400

    Psych (at least in Ruby 1.9.3 up to p429) cannot handle a string of a date format ending in a period.
    This removes @date in favor of a local variable since I could not see any place where the instance variable was used.

--- lib/hatchet/standard_formatter.rb
+++ lib/hatchet/standard_formatter.rb
@@ -56,11 +56,15 @@ module Hatchet
 
       unless secs == @secs
         @secs = secs
-        @date = time.strftime('%Y-%m-%d %H:%M:%S.')
+        # NOTE: this string cannot be marshalled by older versions of Psych so avoid
+        #       placing it in an instance variable
+        date = time.strftime('%Y-%m-%d %H:%M:%S.')
+      else
+        date = ""
       end
 
       @millis = millis
-      @last = @date + "00#{millis}"[-3..-1]
+      @last = date + "00#{millis}"[-3..-1]
     end
 
     # Private: Returns the level formatted for log output as a String.
